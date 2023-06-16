d4c70651a99b0e3f6cd4dab7144904a8bce396d9
Krist <kitog@bk.ru>
2023-06-16 17:17:15 +0400

    Encrypt password with BCrypt and save user to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
 import mongoose from 'mongoose'
 
 import { registerValidation } from './validations/auth.js'
@@ -8,7 +10,7 @@ import { validationResult } from 'express-validator'
 import UserSchema from './models/User.js'
 
 mongoose
-  .connect('mongodb+srv://kristgalstyan417:8520@cluster0.ck2qhkg.mongodb.net/')
+  .connect(process.env.DATA_BASE)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err))
 
@@ -16,15 +18,24 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array())
   }
 
-  res.json({
-    success: true
+  const password = req.body.password
+  const salt = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  const doc = UserSchema({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash
   })
+  const user = await doc.save()
+  res.send(user)
 })
 
 app.listen(process.env.PORT)
