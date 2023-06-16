8fe5f0841b7d9182e051ef1fde1333d33e2a28b2
Krist <kitog@bk.ru>
2023-06-16 13:31:27 +0400

    connected MongoDB

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+import { registerValidation } from './validations/auth.js'
+import { validationResult } from 'express-validator'
+
+import UserSchema from './models/User.js'
+
+mongoose
+  .connect('mongodb+srv://kristgalstyan417:8520@cluster0.ck2qhkg.mongodb.net/')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hell')
-})
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
 
-app.post('/auth/login', (req, res) => {
   res.json({
     success: true
   })
