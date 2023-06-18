407d3c3f636fa2be53b25c5bc75815069ac22ae5
Krist <kitog@bk.ru>
2023-06-18 17:01:58 +0400

    frontEnd

---
+++ front/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import CssBaseline from '@mui/material/CssBaseline'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import './index.scss'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme'
+
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+)
---
+++ front/src/redux/slices/posts.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  posts: {
+    items: [],
+    status: 'loading'
+  },
+  tags: {
+    items: [],
+    status: 'loading'
+  }
+}
